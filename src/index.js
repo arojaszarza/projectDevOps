f1cc528ed942c2d0433655ac6362b10ccf8720fa
Elena <2molina.mariaelena@gmail.com>
2023-02-13 09:41:09 +0100

    Código de Alberto quitado

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-let Elena = "te jodes y arreglas conflictos"
 
-if (Elena === "te jodes y arreglas conflictos"){
-  console.log("Es broma tronca no te lo tomes a mal")
-} else {
-  console.log("Que payasete soy")
-}
-
-app.get('/hello', (req, res) => {
+app.get('/hello', (req, res) => {  
   res.send('Hello World!')
 })
 
