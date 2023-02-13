8979644ac93a8ca5bb528b3a4eb3af4b0882b303
Elena <2molina.mariaelena@gmail.com>
2023-02-13 10:13:50 +0100

    ./src/index.js deleted

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
+var byeRouter = require('./routes/goodbye');
+
 
 var app = express();
 
@@ -21,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
+app.use('/goodbye', byeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
